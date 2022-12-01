5d6529d7da6197cf6b7bfd11cf014bf6b5ef06b0
kauredo <vaskafig@gmail.com>
2022-12-01 12:26:08 +0000

    run locally

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ source "https://rubygems.org"
 
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
-gem "aoc_rb"
+# gem "aoc_rb"
+# gem "aoc_rb", git: 'https://github.com/kauredo/aoc_rb'
+gem "aoc_rb", path: '../aoc_rb'
 gem "pry"
 gem "pry-byebug"
--- spec/2022/01/solution_spec.rb
+++ spec/2022/01/solution_spec.rb
@@ -1,32 +1,45 @@
 # frozen_string_literal: true
 require 'spec_helper'
+require 'pry'
 
 RSpec.describe Year2022::Day01 do
   let(:input) { File.read(File.join(File.dirname(__FILE__), "../../../challenges/2022/01/input.txt")) }
   let(:example_input) {
     <<~EOF
-      change
-      me
+      1000
+      2000
+      3000
+
+      4000
+
+      5000
+      6000
+
+      7000
+      8000
+      9000
+
+      10000
     EOF
   }
 
   describe "part 1" do
-    it "returns nil for the example input" do
-      expect(described_class.part_1(example_input)).to eq(nil)
+    it "returns correct value for the example input" do
+      expect(described_class.part_1(example_input)).to eq(24000)
     end
 
-    it "returns nil for my input" do
-      expect(described_class.part_1(input)).to eq(nil)
+    it "returns correct value for my input" do
+      expect(described_class.part_1(input)).to eq(69289)
     end
   end
 
   describe "part 2" do
-    it "returns nil for the example input" do
-      expect(described_class.part_2(example_input)).to eq(nil)
+    it "returns correct value for the example input" do
+      expect(described_class.part_2(example_input)).to eq(45000)
     end
 
-    it "returns nil for my input" do
-      expect(described_class.part_2(input)).to eq(nil)
+    it "returns correct value for my input" do
+      expect(described_class.part_2(input)).to eq(205615)
     end
   end
 end

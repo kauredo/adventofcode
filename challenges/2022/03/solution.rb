c517c68907a85dd98b5cc97278a16f3adea58171
kauredo <vaskafig@gmail.com>
2022-12-04 19:00:42 +0000

    day 3 2022

---
+++ challenges/2022/03/solution.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+module Year2022
+  class Day03 < Solution
+    # @input is available if you need the raw data input
+    # Call `data` to access either an array of the parsed data, or a single record for a 1-line input file
+    ALPHABET = ('a'..'z').to_a + ('A'..'Z').to_a
+    def part_1
+      total = 0
+      numbers = {}
+      data.each do |line|
+        first = line.split('').first(line.length / 2)
+        last = line.split('').last(line.length / 2)
+        common = (first & last).first
+
+        number = ALPHABET.index(common) + 1
+
+        numbers[common] = number
+        total += number
+      end
+      total
+    end
+
+    def part_2
+      total = 0
+      numbers = {}
+      data.each_slice(3) do |lines|
+        lines.map!{|line| line.split('')}
+        common = lines.reduce(:&).first
+        number = ALPHABET.index(common) + 1
+
+        numbers[common] = number
+        total += number
+      end
+      total
+    end
+
+    private
+      # Processes each line of the input file and stores the result in the dataset
+      # def process_input(line)
+      #   line.map(&:to_i)
+      # end
+
+      # Processes the dataset as a whole
+      # def process_dataset(set)
+      #   set
+      # end
+  end
+end
---
+++ spec/2022/03/solution_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+require 'spec_helper'
+
+RSpec.describe Year2022::Day03 do
+  let(:input) { File.read(File.join(File.dirname(__FILE__), "../../../challenges/2022/03/input.txt")) }
+  let(:example_input) {
+    <<~EOF
+      vJrwpWtwJgWrhcsFMMfFFhFp
+      jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+      PmmdzqPrVvPwwTWBwg
+      wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+      ttgJtRGJQctTZtZT
+      CrZsJsPPZsGzwwsLwLmpwMDw
+    EOF
+  }
+
+  describe "part 1" do
+    it "returns nil for the example input" do
+      expect(described_class.part_1(example_input)).to eq(157)
+    end
+
+    it "returns nil for my input" do
+      expect(described_class.part_1(input)).to eq(7875)
+    end
+  end
+
+  describe "part 2" do
+    it "returns nil for the example input" do
+      expect(described_class.part_2(example_input)).to eq(70)
+    end
+
+    it "returns nil for my input" do
+      expect(described_class.part_2(input)).to eq(2479)
+    end
+  end
+end
